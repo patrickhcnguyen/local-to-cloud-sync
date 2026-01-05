febb1a624b0787600e10922f456534a97574e018
patrickhcnguyen <patricknguyen714@gmail.com>
2026-01-04 17:39:48 -0800

    basic sqlite engine

---
+++ engine/storage/sqlite.ts
@@ -0,0 +1,80 @@
+import sqlite3 from 'sqlite3';
+import fs from 'fs';
+import path from 'path';
+import { NoteWithContent } from '../models/notes';
+
+const DB_PATH = path.join(process.cwd(), 'data', 'notes.db');
+
+let db: sqlite3.Database 
+
+export function openDB() {
+    if (!fs.existsSync("data")) {
+        fs.mkdirSync("data");
+    }
+
+    db = new sqlite3.Database(DB_PATH);
+    db.run(fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8'));
+    return db;
+}
+
+export function runSchema(schema: string) {
+    return new Promise<void>((resolve, reject) => {
+        db.exec(schema, err => (err ? reject(err) : resolve()));
+    });
+}
+
+export function insertNote(note: Note) {
+    return new Promise<void>((resolve, reject) => {
+        db.run(
+            `INSERT INTO notes (id, title, content, created_at, updated_at) VALUES(?, ?, ?, ?, ?)`,
+            [
+                note.id,
+                note.title,
+                note.content,
+                note.createdAt.getTime(),   
+                note.updatedAt.getTime(),
+            ],
+            (err) => (err ? reject(err) : resolve())
+        );
+    });
+}
+
+export function updateNoteContent(id: number, content: string, updatedAt: Date) {
+    return new Promise<void>((resolve, reject) => {
+        db.run(
+            `UPDATE notes SET content = ?, updated_at = ? WHERE id = ?`,
+            [content, updatedAt.getTime(), id],
+            err => (err ? reject(err) : resolve())
+        );
+    });
+}
+
+export function deleteNote(id: number) {
+    return new Promise<void>((resolve, reject) => {
+        db.run(
+            `DELETE FROM notes WHERE id = ?`,
+            [id],
+            err => (err ? reject(err) : resolve())
+        );
+    });
+}
+
+export function fetchAllNotes() {
+    return new Promise<NoteWithContent[]>((resolve, reject) => {
+        db.all(
+            `SELECT * FROM notes`,
+            [],
+            (err, rows) => (err ? reject(err) : resolve(rows as NoteWithContent[]))
+        );
+    });
+}
+
+export function fecthNoteByID(id: number) {
+    return new Promise<NoteWithContent | undefined>((resolve, reject) => {
+        db.get(
+            `SELECT * FROM notes WHERE id = ?`,
+            [id],
+            (err, row) => (err ? reject(err) : resolve(row as NoteWithContent))
+        );
+    });
+}
